3d1ed5a2ffd7e03db203df2907e1c00972a8d44d
Dusan Miljkovic <dusan@dedaldev.com>
2021-09-10 20:09:40 +0200

    docs

--- back-end/employee-service/src/main/java/com/example/employeeservice/repository/EmployeeRepository.java
+++ back-end/employee-service/src/main/java/com/example/employeeservice/repository/EmployeeRepository.java
@@ -6,10 +6,22 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+/**
+ * EMployee repository.
+ */
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
+    /**
+     * Get all employees by department id.
+     * @param departmentId Department id.
+     * @return List of employees.
+     */
     List<Employee> getEmployeesByDepartmentId(Long departmentId);
 
+    /**
+     * Get all employees by organization id.
+     * @param organizationId Organization id.
+     * @return List of employees.
+     */
     List<Employee> getEmployeesByOrganizationId(Long organizationId);
 }
--- back-end/employee-service/src/main/java/com/example/employeeservice/service/EmployeeService.java
+++ back-end/employee-service/src/main/java/com/example/employeeservice/service/EmployeeService.java
@@ -11,17 +11,33 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Employee service.
+ */
 @Service
 @AllArgsConstructor
 public class EmployeeService {
 
+    /**
+     * Instance od employee repository.
+     */
     private final EmployeeRepository employeeRepository;
 
+    /**
+     * Add employee.
+     * @param employee Employee.
+     * @return Added employee.
+     */
     public Employee add(Employee employee) {
         employeeRepository.save(employee);
         return employee;
     }
 
+    /**
+     * Get employee by id.
+     * @param id Employee id.
+     * @return Employee.
+     */
     public ResponseEntity<Employee> findById(Long id) {
         Employee employee = employeeRepository.findById(id).orElseThrow(() -> new IllegalStateException(
                 "Employee with id " + id + " does not exist."
@@ -29,18 +45,37 @@ public class EmployeeService {
         return ResponseEntity.ok(employee);
     }
 
+    /**
+     * Get all employees.
+     * @return List of employees.
+     */
     public List<Employee> findAll() {
         return employeeRepository.findAll();
     }
 
+    /**
+     * Find all employees by department id.
+     * @param departmentId Department id.
+     * @return List of employees.
+     */
     public List<Employee> findByDepartment(Long departmentId) {
         return employeeRepository.getEmployeesByDepartmentId(departmentId);
     }
 
+    /**
+     * Get all employees by organization id.
+     * @param organizationId Organization id.
+     * @return List of employees.
+     */
     public List<Employee> findByOrganization(Long organizationId) {
         return employeeRepository.getEmployeesByOrganizationId(organizationId);
     }
 
+    /**
+     * Delete employee.
+     * @param id Employee id.
+     * @return Response entity.
+     */
     public ResponseEntity<Map<String, Boolean>> deleteById(Long id) {
         boolean exists = employeeRepository.existsById(id);
         if(!exists){
@@ -52,6 +87,11 @@ public class EmployeeService {
         return ResponseEntity.ok(response);
     }
 
+    /**
+     * Delete employees by department id.
+     * @param id Department id.
+     * @return Response.
+     */
     public ResponseEntity<Map<String, Boolean>> deleteByDepartmentId(Long id) {
         List<Employee> employees = employeeRepository.getEmployeesByDepartmentId(id);
         employees.forEach( employee -> {employeeRepository.deleteById(employee.getId());});
@@ -61,6 +101,11 @@ public class EmployeeService {
     }
 
 
+    /**
+     * Detelete employees by organization id.
+     * @param organizationId Organization id.
+     * @return Response entity.
+     */
     public ResponseEntity<Map<String, Boolean>> deleteByOrganizationId(Long id) {
         List<Employee> employees = employeeRepository.getEmployeesByOrganizationId(id);
         employees.forEach( employee -> {employeeRepository.deleteById(employee.getId());});
@@ -69,6 +114,12 @@ public class EmployeeService {
         return ResponseEntity.ok(response);
     }
 
+    /**
+     * Uodate employee.
+     * @param id Employee id.
+     * @param employeeDetails Employee details.
+     * @return Updated employee.
+     */
     @Transactional
     public ResponseEntity<Employee> update(Long id, Employee employeeDetails) {
         Employee employee = employeeRepository.findById(id).orElseThrow(() -> new IllegalStateException(
